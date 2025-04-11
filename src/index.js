34a9e8027c9ceccad9fd50824c3e8e42ec9f34fa
Kristinn F Birgisson <88903567+Kristinnfb@users.noreply.github.com>
2025-04-11 12:16:19 +0000

    Completed

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const noMomentRule = require('./rules/no-moment');
+
+module.exports = {
+    rules: {
+        'no-moment': noMomentRule
+    },
+    configs: {
+        recommended: {
+            plugins: ['no-moment'],
+            rules: {
+                'no-moment/no-moment': 'error'
+            }
+        }
+    }
+}; 
